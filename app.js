6a1420e4204d9b95ccc67aff46d94ad9ec13033d
mymymy1303 <thehuy160897@gmail.com>
2018-09-22 10:34:29 +0700

    Create source

---
+++ app.js
@@ -0,0 +1,75 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var sassMiddleware = require('node-sass-middleware');
+// Gọi Route
+
+import indexRouter from './routes/index.route'
+import aboutRouter from './routes/about.route'
+import userRouter from './routes/users.route'
+import usersDetailsRouter from './routes/usersdetails.route'
+import formRouter from './routes/form.route'
+import formSave from './routes/save.route'
+
+// Khởi tạo APP
+var app = express();
+// Call DB //////////////////////
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var __data = 'mongodb://localhost:27017/thay_nguyen';
+mongoose.connect(__data, {useNewUrlParser: true});
+// Global
+mongoose.Promise = global.Promise;
+//Lấy kết nối mặc định
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Lỗi kết nối CSDL'));
+// Call DB //////////////////////
+// Gọi Template
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+// Cấu hình
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'styles'),
+  dest: path.join(__dirname, 'public/css'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: app.get('env') === 'development' ? false : true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+// Init App
+app.use('/', indexRouter)
+app.use('/about', aboutRouter)
+app.use('/users', userRouter)
+app.use('/user', usersDetailsRouter)
+app.use('/form', formRouter)
+app.use('/save', formSave)
+
+
+app.get('/logout', function (req, res, next) {
+	req.logout()
+	res.redirect("/")
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	if(req.app.get('env') === 'dev') {
+		next(createError(404));
+	} else{
+		res.status(400);
+		res.render('404.pug', { title: "404 We're sorry!", desc: "We couldn't find what you're looking for", btn: "» Go back to the main page" });
+	}
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/save.route.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import crypto from 'crypto'
+import User from '../models/user.model'
+const router = express.Router();
+
+router.post('/', (req, res, next) => {
+	let {
+		Username,
+		EmailAddress
+	} = req.body;
+	let hashKey = crypto.randomBytes(5).toString('hex') + '-' + crypto.randomBytes(10).toString('hex') + '-' + crypto.randomBytes(5).toString('hex') + '-' + crypto.randomBytes(5).toString('hex');
+	let hashPass = bcrypt.hashSync(EmailAddress, bcrypt.genSaltSync(8), null);
+
+	let myData = new User({
+		key: hashKey,
+		username: Username,
+		email: EmailAddress,
+		hash: hashPass
+	});
+	myData.save()
+		.then(item => {
+			res.redirect("/users")
+			// res.send(item); // Chỉ bật lên khi viết APIs
+		})
+		.catch(err => {
+			next(err);
+			res.status(400).send("unable to save to database");
+		});
+})
+
+export default router;
